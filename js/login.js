0e49e2ba1c10ec8e60b82e61f995317789b646a8
kble <kble326@gmail.com>
2021-05-09 17:16:38 +0800

    standardize nav bar some bug fix

--- js/login.js
+++ js/login.js
@@ -1,44 +1,9 @@
-var emailArray=[];
-var passwordArray=[];
-
-var loginBox = document.getElementById("login");
-var regBox = document.getElementById("register");
-var forgetBox = document.getElementById("forgot");
+var emailArray=['a@example.com'];
+var passwordArray=['1234'];
 
 var loginTab = document.getElementById("lt");
 var regTab = document.getElementById("rt");
 
-function regTabFun(){
-    event.preventDefault();
-
-    regBox.style.visibility="visible";
-    loginBox.style.visibility="hidden";
-    forgetBox.style.visibility="hidden";
-
-    regTab.style.backgroundColor="rgb(12, 132, 189)";
-    loginTab.style.backgroundColor="rgba(11, 177, 224, 0.82)";
-}
-function loginTabFun(){
-    event.preventDefault();
-
-    regBox.style.visibility="hidden";
-    loginBox.style.visibility="visible";
-    forgetBox.style.visibility="hidden";
-
-    loginTab.style.backgroundColor="rgb(12, 132, 189)";
-    regTab.style.backgroundColor="rgba(11, 177, 224, 0.82)";
-}
-function forTabFun(){
-    event.preventDefault();
-
-    regBox.style.visibility="hidden";
-    loginBox.style.visibility="hidden";
-    forgetBox.style.visibility="visible";
-
-    regTab.style.backgroundColor="rgba(11, 177, 224, 0.82)";
-    loginTab.style.backgroundColor="rgba(11, 177, 224, 0.82)";
-
-}
 
 
 function register(){
@@ -104,28 +69,11 @@ function login(){
         return ;
     }
     else {
-        alert(email + " yor are login Now \n welcome to our website.");
+        window.location.href='../member';
 
         document.getElementById("se").value ="";
         document.getElementById("sp").value="";
         return ;
     }
 
-}
-function forgot(){
-    event.preventDefault();
-
-    var email = document.getElementById("fe").value;
-
-    if(emailArray.indexOf(email) == -1){
-        if (email == ""){
-            alert("Email required.");
-            return ;
-        }
-        alert("Email does not exist.");
-        return ;
-    }
-
-    alert("email is send to your email check it in 24hr. \n Thanks");
-    document.getElementById("fe").value ="";
 }
